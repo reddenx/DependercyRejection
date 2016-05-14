07c39348706202a75bbc07dd169719eb659cd0c3
SM_D <seanmarek@gmail.com>
2016-05-14 15:25:30 -0700

    is it usable?

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -23,14 +23,14 @@ namespace ConsoleApplication1
             if (File.Exists(saveFileName))
             {
                 Console.WriteLine("Loading From Cache");
-                dependencies = LoadFromFile(saveFileName);
+                dependencies = DependencyGraphFactory.LoadFromFile(saveFileName);
             }
 
             //if save didn't populate
             if (dependencies == null)
             {
-                dependencies = BuildFromDisk(devFolder);
-                SaveToFile(saveFileName, dependencies);
+                dependencies = DependencyGraphFactory.BuildFromDisk(devFolder);
+                DependencyGraphFactory.SaveToFile(saveFileName, dependencies);
             }
 
 
@@ -49,7 +49,7 @@ namespace ConsoleApplication1
                 if (consoleInput.Contains("--refresh"))
                 {
                     //refresh here
-                    BuildFromDisk(devFolder);
+                    DependencyGraphFactory.BuildFromDisk(devFolder);
                     continue;
                 }
 
@@ -64,8 +64,8 @@ namespace ConsoleApplication1
 				{
 
 					//based on input file, 
-					var upTheGraph = GetDependantsForProject(selectedProjects.ToArray());
-					var downTheGraph = GetProjectDependencies(selectedProjects.ToArray(), string.Empty, verbose);
+                    var upTheGraph = DependencyGraphFactory.GetDependantsForProject(selectedProjects.ToArray());
+                    var downTheGraph = DependencyGraphFactory.GetProjectDependencies(selectedProjects.ToArray(), string.Empty, verbose);
 
 					Console.WriteLine(
 		@"
@@ -73,7 +73,7 @@ namespace ConsoleApplication1
 Dependent Solutions
 ===================");
 					int index = 1;
-					foreach (var solution in upTheGraph.Item1.Distinct().OrderBy(sol => sol.FilePath))
+					foreach (var solution in upTheGraph.SolutionFiles.Distinct().OrderBy(sol => sol.FilePath))
 					{
 						Console.WriteLine("{0} - {1}", index++, solution.FilePath);
 					}
@@ -83,7 +83,7 @@ Dependent Solutions
 Dependent Projects
 ==================");
 					index = 1;
-					foreach (var project in upTheGraph.Item2.Distinct().OrderBy(proj => proj.AssemblyName))
+					foreach (var project in upTheGraph.ProjectFiles.Distinct().OrderBy(proj => proj.AssemblyName))
 					{
 						Console.WriteLine("{0} - {1}", index++, project.AssemblyName);
 					}
@@ -103,108 +103,5 @@ Project Requires
 			}
 			while (consoleInput != "exit");
 		}
-
-        private static DependencyGraph BuildFromDisk(string devFolder)
-        {
-            Console.WriteLine("Gathering Projects From: {0}", devFolder);
-            var projectsFilePaths = Directory.GetFiles(devFolder, "*.csproj", SearchOption.AllDirectories);
-            Console.WriteLine("Found: {0}", projectsFilePaths.Length);
-
-            Console.WriteLine("Gathering Solutions From: {0}", devFolder);
-            var solutionFilePaths = Directory.GetFiles(devFolder, "*.sln", SearchOption.AllDirectories);
-            Console.WriteLine("Found: {0}", solutionFilePaths.Length);
-
-            Console.WriteLine("Constructing Data", projectsFilePaths.Count());
-            var projects = projectsFilePaths.Select(projectPath => ProjectFile.BuildFromFile(projectPath)).ToArray();
-            var solutions = solutionFilePaths.Select(solutionPath => SolutionFile.BuildFromFile(solutionPath, projects)).ToArray();
-
-            Console.WriteLine("Building Dependency Graph, {0} operations expected", projects.Length * projects.Length * solutions.Length);
-            //build dependency graph, could be done in linq but meh
-            foreach (var outerProject in projects)
-            {
-                foreach (var innerProject in projects)
-                {
-                    if (outerProject.ReferencesProjectIds.Contains(innerProject.ProjectId))
-                    {
-                        outerProject.ReferencesProjects.Add(innerProject);
-                        innerProject.ReferencedByProjects.Add(outerProject);
-                    }
-                }
-            }
-            return new DependencyGraph(projects, solutions);
-        }
-
-        private static DependencyGraph LoadFromFile(string saveFileName)
-        {
-            var formatter = new BinaryFormatter();
-            try
-            {
-                using (var inStream = File.OpenRead(saveFileName))
-                {
-                    return (DependencyGraph)formatter.Deserialize(inStream);
-                }
-            }
-            catch
-            {
-                return null;
-            }
-        }
-
-        private static void SaveToFile(string saveFileName, DependencyGraph dependencies)
-        {
-            Console.WriteLine("Complete, saving to: {0}\r\n --refresh to refresh from disk", saveFileName);
-            try
-            {
-                try { File.Delete(saveFileName); }
-                catch { }
-
-                using (var stream = File.OpenWrite(saveFileName))
-                {
-                    var formatter = new BinaryFormatter();
-                    formatter.Serialize(stream, dependencies);
-                }
-
-            }
-            catch
-            {
-                Console.WriteLine("Unable to save file: {0}", saveFileName);
-            }
-        }
-
-		private static Tuple<SolutionFile[], ProjectFile[]> GetDependantsForProject(ProjectFile[] inputProjects)
-		{
-			var solutions = new List<SolutionFile>(inputProjects.SelectMany(proj => proj.ReferencedBySolutions));
-			var projects = new List<ProjectFile>(inputProjects.SelectMany(proj => proj.ReferencedByProjects));
-
-			foreach (var dependentProject in inputProjects.SelectMany(proj => proj.ReferencedByProjects))
-			{
-				var dependants = GetDependantsForProject(new[] { dependentProject });
-				solutions.AddRange(dependants.Item1);
-				projects.AddRange(dependants.Item2);
-			}
-
-			return new Tuple<SolutionFile[], ProjectFile[]>(solutions.ToArray(), projects.ToArray());
-		}
-
-		private static string[] GetProjectDependencies(ProjectFile[] inputProjects, string treeBaseName, bool verbose)
-		{
-			var projects = new List<ProjectFile>(inputProjects.SelectMany(proj => proj.ReferencesProjects));
-			var dependencies = new List<string>();
-
-			foreach (var reference in inputProjects.SelectMany(proj => proj.ReferencesProjects))
-			{
-				var treeBase = reference.AssemblyName;
-				if (verbose)
-				{
-					treeBase = treeBaseName + "->" + reference.AssemblyName;
-				}
-				var references = GetProjectDependencies(new[] { reference }, treeBase, verbose);
-				dependencies.Add(treeBase);
-				dependencies.AddRange(references);
-			}
-
-			return dependencies.ToArray();
-		}
-
 	}
 }
--- ConsoleApplication1/DependercyRejectionUI/Form1.cs
+++ ConsoleApplication1/DependercyRejectionUI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApplication1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,7 @@ namespace DependercyRejectionUI
         private enum LoadState
         {
             NotLoaded,
+            Loading,
             Loaded,
         }
 
@@ -25,23 +27,85 @@ namespace DependercyRejectionUI
             set
             {
                 _currentLoadState = value;
-                if (_currentLoadState == LoadState.Loaded)
+                switch (_currentLoadState)
                 {
-                    SetAssemblyControls(true);
-                }
-                else
-                {
-                    SetAssemblyControls(false);
+                    case LoadState.Loaded:
+                        SetAssemblyControls(true);
+                        break;
+                    default:
+                        SetAssemblyControls(false);
+                        break;
                 }
             }
         }
 
+        private DependencyGraph DependencyGraph;
 
         public Form1()
         {
             InitializeComponent();
 
             CurrentLoadState = LoadState.NotLoaded;
+
+            Button_BuildFromDirectory.Click += Button_BuildFromDirectory_Click;
+            Button_LoadAssemblyInformation.Click += Button_LoadAssemblyInformation_Click;
+        }
+
+        private void Button_LoadAssemblyInformation_Click(object sender, EventArgs e)
+        {
+            if (ComboBox_AssemblySelector.SelectedItem is ComboBoxItem && ((ComboBoxItem)ComboBox_AssemblySelector.SelectedItem) != null)
+            {
+                BuildTreeInfo(((ComboBoxItem)ComboBox_AssemblySelector.SelectedItem).Value);
+            }
+        }
+
+        private void BuildTreeInfo(ProjectFile projectFile)
+        {
+            TreeView_AssemblyInformationTree.Nodes.Clear();
+            var baseNodes = BuildTreeNodes(projectFile);
+            TreeView_AssemblyInformationTree.Nodes.Add(baseNodes.Item1);
+
+            //traverse tree time...
+        }
+
+        private Tuple<TreeNode, int> BuildTreeNodes(ProjectFile file)
+        {
+            var thisNode = new TreeNode();
+
+            int projectCount = file.ReferencesProjects.Count;
+            foreach (var project in file.ReferencesProjects)
+            {
+                var info = BuildTreeNodes(project);
+                thisNode.Nodes.Add(info.Item1);
+                projectCount += info.Item2;
+            }
+
+            thisNode.Text = (string.Format("{0}: {1}", file.AssemblyName, projectCount));
+
+            return new Tuple<TreeNode, int>(thisNode, projectCount);
+        }
+
+        private void Button_BuildFromDirectory_Click(object sender, EventArgs e)
+        {
+            DependencyGraph = ConsoleApplication1.DependencyGraphFactory.BuildFromDisk(TextBox_DirectoryInputText.Text);
+
+            if (DependencyGraph != null)
+            {
+                PopulateComboBox();
+                CurrentLoadState = LoadState.Loaded;
+            }
+        }
+
+        private void PopulateComboBox()
+        {
+            ComboBox_AssemblySelector.Items.AddRange(DependencyGraph.ProjectFiles.Select(project =>
+                {
+                    return new ComboBoxItem()
+                    {
+                        Text = project.AssemblyName,
+                        Value = project
+                    };
+                }).ToArray());
         }
 
         private void SetAssemblyControls(bool enabled)
@@ -50,6 +114,17 @@ namespace DependercyRejectionUI
             ComboBox_AssemblySelector.Enabled = enabled;
             Button_LoadAssemblyInformation.Enabled = enabled;
             TreeView_AssemblyInformationTree.Enabled = enabled;
+        }
+
+        private class ComboBoxItem
+        {
+            public string Text { get; set; }
+            public ProjectFile Value { get; set; }
+
+            public override string ToString()
+            {
+                return Text;
+            }
         }
     }
 }
